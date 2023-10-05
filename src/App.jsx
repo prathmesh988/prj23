42d9774ab2ac6f069aca2259161127e4c73449bd
Orcus <kusum.aayushi@gmail.comcls>
2023-10-05 23:34:21 +0530

    added many things

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,67 @@
-import { useState } from 'react'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import reactLogo from './assets/react.svg'
+import { useState } from "react";
+import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
+import reactLogo from "./assets/react.svg";
+import image from "./assets/landingpagephoto.png";
+import LoGin from "./Pages/Login";
+import SignUp from "./Pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Heads from "./pages/ResetPassword";
+import OrderLabel from "./pages/OrderLabel";
+import Orders from "./pages/Orders";
 
-import image from './assets/landingpagephoto.png';
-
-import LoGin from './Pages/Login';
-import SignUp from './Pages/SignUp';
-import Heads from './Pages/ResetPassword';
+function Home() {
+  return (
+    <div className=" flex gap-4 w-[100vw] h-[100vh] justify-center items-center">
+      <Link to={"/Dashboard"}>
+        <button className="bg-gray-200 rounded-xl px-2 py-1">Dasboard</button>
+      </Link>
+      <Link to={"/login"}>
+        <button className="bg-gray-200 rounded-xl px-2 py-1">Login</button>
+      </Link>
+      <Link to={"/signup"}>
+        <button className="bg-gray-200 rounded-xl px-2 py-1">signup</button>
+      </Link>
+    </div>
+  );
+}
 
 const router = createBrowserRouter([
   {
+    path: "/",
+    element: <Home />,
+  },
+  {
     path: "/login",
-    element: <LoGin/>,
-   
-   
+    element: <LoGin />,
   },
   {
-    path:'/resetpassword',
-    element:<Heads/>
+    path: "/resetpassword",
+    element: <Heads />,
   },
   {
-    path:'/signup',
-    element:<SignUp/>
+    path: "/signup",
+    element: <SignUp />,
+  },
+  {
+    path: "/Dashboard",
+    element: <Dashboard />,
+  },
+  {
+    path: "/OrderLabel",
+    element: <OrderLabel />,
+  },
+  {
+    path: "/Orders",
+    element: <Orders />,
   },
 ]);
 
-//You can change the routes according to your outlest coinfguraation 
-//like where you waant to place your child components of that routes using outlet
-
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
-    <>  
-    <RouterProvider router={router} />
+    <>
+      <RouterProvider router={router} />
     </>
-      
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Pages/Dashboard.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import Sidebar from "../components/Sidebar";
+
+function DashboardSection() {
+  return (
+    <section className="flex-grow">
+      <div className="py-4 border-b-2 px-6">
+        <div>
+          <h1 className="text-2xl font-bold">Dashboard</h1>
+          <p className="text-xs">Check your progress</p>
+        </div>
+      </div>
+
+      <section className="px-6 py-2">
+        <section className="w-4/5 shadow-lg px-6 py-4 flex justify-between">
+          <div className="">
+            <h3 className="text-sm font-semibold text-gray-500">Balance</h3>
+            <div className="h-[60px] border-r-2 flex align-bottom w-[200px]">
+              <h1 className="font-bold text-lg">$280.00</h1>
+            </div>
+            <p className="text-xs text-gray-500 font-bold">Last Week $0</p>
+          </div>
+          <div className="">
+            <h3 className="text-sm font-semibold text-gray-500">Balance</h3>
+            <div className="h-[60px] border-r-2 flex align-bottom w-[200px]">
+              <h1 className="font-bold text-lg">$280.00</h1>
+            </div>
+            <p className="text-xs text-gray-500 font-bold">Last Week $0</p>
+          </div>
+          <div className="">
+            <h3 className="text-sm font-semibold text-gray-500">Balance</h3>
+            <div className="h-[60px] border-r-2 flex align-bottom w-[200px]">
+              <h1 className="font-bold text-lg">$280.00</h1>
+            </div>
+            <p className="text-xs text-gray-500 font-bold">Last Week $0</p>
+          </div>
+          <div className="">
+            <h3 className="text-sm font-semibold text-gray-500">Balance</h3>
+            <div className="h-[60px] border-r-2 flex align-bottom w-[200px]">
+              <h1 className="font-bold text-lg">$280.00</h1>
+            </div>
+            <p className="text-xs text-gray-500 font-bold">Last Week $0</p>
+          </div>
+        </section>
+
+        <section className="w-4/5 shadow-lg px-6 py-4">
+          <div></div>
+        </section>
+      </section>
+    </section>
+  );
+}
+
+const Dashboard = () => {
+  return (
+    <div className="flex ">
+      <Sidebar />
+      <DashboardSection />
+    </div>
+  );
+};
+
+export default Dashboard;
