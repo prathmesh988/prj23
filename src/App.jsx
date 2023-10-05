4d8ad83edd3a2af1ab71cb6e42b5e5d244c30c3a
prathmesh988 <prathmeshgawade41@gmail.com>
2023-10-05 12:06:22 +0530

    added the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
 import { useState } from 'react'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import reactLogo from './assets/react.svg'
 
 import image from './assets/landingpagephoto.png';
 
 import LoGin from './Pages/Login';
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <LoGin/>
+   
+  },
+]);
+
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>  
-    <LoGin/>
+    <RouterProvider router={router} />
     </>
       
   )
---
+++ src/Pages/SignUp.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+
+// import image from './assets/landingpagephoto.png';
+
+function SignUp(){
+    return(
+        <>
+            <div className='landingpage w-[100%] h-[100%]'>
+
+            </div>
+        </>
+    )
+}
